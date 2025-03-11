f3ea5774aa6337c5284b63475bb8e4f2c21dacef
csvidit <63170797+csvidit@users.noreply.github.com>
2025-03-11 19:48:31 -0400

    (src/utils) add font and tw utils

---
+++ src/utils/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Instrument_Serif } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const instrumentSerif = Instrument_Serif({
+    subsets: ["latin"],
+    weight: "400"
+});
