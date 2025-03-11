7271a4ff1dbb2ace01777854a9757c318ba33feb
csvidit <63170797+csvidit@users.noreply.github.com>
2025-03-11 19:50:07 -0400

    (app/ page.tsx and layout.tsx) remove boilerplate and add components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import BackgroundPattern from "@/components/background/BackgroundPattern";
+import MainContainer from "@/components/containers/MainContainer";
+import '@fontsource-variable/inclusive-sans';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,16 +11,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <MainContainer>
+        <BackgroundPattern color="text-slate-900" />
         {children}
-      </body>
+      </MainContainer>
     </html>
   );
 }
