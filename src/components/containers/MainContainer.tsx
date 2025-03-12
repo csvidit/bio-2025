2c7b07358e868f1112d2d4ddc99e5be407184c1c
csvidit <63170797+csvidit@users.noreply.github.com>
2025-03-11 23:40:58 -0400

    change font

--- src/components/containers/MainContainer.tsx
+++ src/components/containers/MainContainer.tsx
@@ -1,11 +1,11 @@
 import { cn } from "@/utils/css-utils";
-import { inter } from "@/utils/fonts";
+import { gantari } from "@/utils/fonts";
 
 const MainContainer = (props: {
 	children: React.ReactNode;
 	className?: string;
 }) => {
-	return <body className={cn("flex text-black flex-col justify-center items-center w-screen max-w-screen min-h-screen h-full bg-transparent", props.className, inter.className)}>{props.children}</body>;
+	return <body className={cn("flex text-black flex-col justify-center items-center w-screen max-w-screen min-h-screen h-full bg-transparent", props.className, gantari.className)}>{props.children}</body>;
 };
 
 export default MainContainer;
--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,7 +1,12 @@
-import { Inter, Instrument_Serif } from "next/font/google";
+import { Inter, Instrument_Serif, Gantari } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 export const instrumentSerif = Instrument_Serif({
+    subsets: ["latin"],
+    weight: "400"
+});
+
+export const gantari = Gantari({
     subsets: ["latin"],
     weight: "400"
 });
